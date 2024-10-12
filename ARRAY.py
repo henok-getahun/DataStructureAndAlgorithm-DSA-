e4afebb92c74fc2bd5270dd26f0728d658b48e15
HENOK GETAHUN <162030691+henok-getahun@users.noreply.github.com>
2024-10-12 05:01:49 +0300

    Update ARRAY.py

--- ARRAY.py
+++ ARRAY.py
@@ -1,46 +1,46 @@
-#--------[   ARRAY  ]-----------
-
 from array import array
 
-arr = array('i', [1, 3, 5, 6, 7])
+class ArrayOperations:
+    def __init__(self, initial_data):
+        self.arr = array('i', initial_data)
 
-def traversal(arr):
-    for x in arr:
-        print(x, end=' ')
-    print()  
+    def traversal(self):
+        for x in self.arr:
+            print(x, end=' ')
+        print()
 
-def insertion(arr, idx, val):
-    n = len(arr)
-    new_arr = array(arr.typecode, [0] * (n + 1))  
-    for i in range(idx):
-        new_arr[i] = arr[i]
-    new_arr[idx] = val  
-    for i in range(idx, n):
-        new_arr[i + 1] = arr[i]
-    return new_arr
+    def insertion(self, idx, val):
+        n = len(self.arr)
+        new_arr = array(self.arr.typecode, [0] * (n + 1))
+        for i in range(idx):
+            new_arr[i] = self.arr[i]
+        new_arr[idx] = val
+        for i in range(idx, n):
+            new_arr[i + 1] = self.arr[i]
+        self.arr = new_arr
 
-def deletion(arr, val):
-    idx = search(arr, val)
-    n = len(arr)
-    if idx == -1: 
-        return arr  
-    new_arr = array(arr.typecode, [0] * (n - 1))
-    for i in range(idx):
-        new_arr[i] = arr[i]
-    for i in range(idx, n - 1):
-        new_arr[i] = arr[i + 1]
-    return new_arr
+    def deletion(self, val):
+        idx = self.search(val)
+        n = len(self.arr)
+        if idx == -1:
+            return
+        new_arr = array(self.arr.typecode, [0] * (n - 1))
+        for i in range(idx):
+            new_arr[i] = self.arr[i]
+        for i in range(idx, n - 1):
+            new_arr[i] = self.arr[i + 1]
+        self.arr = new_arr
 
-def search(arr, val):
-    n = len(arr)
-    for i in range(n):
-        if arr[i] == val:  
-            return i
-    return -1
+    def search(self, val):
+        n = len(self.arr)
+        for i in range(n):
+            if self.arr[i] == val:
+                return i
+        return -1
 
-
-traversal(arr)
-arr = insertion(arr, 2, 4)  
-traversal(arr)
-arr = deletion(arr, 5) 
-traversal(arr)
+array_ops = ArrayOperations([1, 3, 5, 6, 7])
+array_ops.traversal()
+array_ops.insertion(2, 4)
+array_ops.traversal()
+array_ops.deletion(5)
+array_ops.traversal()
